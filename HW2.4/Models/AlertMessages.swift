f3eda1504bd9e8bc524ae03118aefddc92e80f32
MaximShvanov <shvanov.m@yandex.ru>
2020-08-28 21:37:57 +0300

    -- bug -- fix

---
+++ HW2.4/Models/AlertMessages.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  HW2.4
+//
+//  Created by Maxon on 28.08.2020.
+//  Copyright © 2020 Maxim Shvanov. All rights reserved.
+//
+
+import Foundation
+
+enum AlertMessages: String {
+    case enterName = "Please, enter your name!"
+    case enterPassword = "Please, enter your password!"
+    case badLogin = "Login or password is incorrect!"
+}
--- HW2.4/ViewControllers/ViewController.swift
+++ HW2.4/ViewControllers/ViewController.swift
@@ -17,19 +17,22 @@ class ViewController: UIViewController {
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
+    var alertController: UIAlertController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func loginButtonTapped() {
-        if nameTextField.text == nil || nameTextField.text == "" {
-            print("Please, enter your login!")
-            return
-        }
+        (nameTextField.text == nil || nameTextField.text == "") ? alertCall(message: "Please, enter your name!") : print("YES")
+//        if nameTextField.text == nil || nameTextField.text == "" {
+//            alertCall(title: "Error!", message: "Please, enter your login!")
+//            return
+//        }
         
         if passwordTextField.text == nil || passwordTextField.text == "" {
-            print("Please, enter your password!")
+            alertCall(message: "Please, enter your password!")
             return
         }
         
@@ -37,20 +40,30 @@ class ViewController: UIViewController {
             if password == passwordTextField.text! {
                 performSegue(withIdentifier: "welcomeUserSegue", sender: nil)
             } else {
-                print("Password incorrect!")
+                alertCall(message: "Login or password is incorrect!")
             }
         } else {
-            print("Login incorrect!")
+            alertCall(message: "Login or password is incorrect!")
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let welcomeDestinationVC = segue.destination as? WelcomeUserViewController else { return }
+//        guard let welcomeDestinationVC = segue.destination as? WelcomeUserViewController else { return }
          
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    private func alertCall(message: String) -> Void {
+        alertController = UIAlertController(title: "Error!", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured")
+        }))
+        present(alertController, animated: true, completion: nil)
+        
+        return
     }
 }
 
